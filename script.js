38ef46b228de5ae54b68cb034f43d979209683b2
Luccas Carvalho <49130817+Luccas-carvalho@users.noreply.github.com>
2024-01-08 10:20:33 -0300

    Add cart products
    Adicionado carrinho de compras, ainda estático, em breve será funcional.

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ menuToggle.addEventListener('click', () => {
     show = !show;
 
 });
+
+
+const menuCarToggle = document.querySelector('.bi.bi-cart2'); // Ícone do carrinho
+const closeCartIcon = document.querySelector('.bi.bi-arrow-left'); // Ícone para fechar o carrinho
+const carrinho = document.querySelector('.carrinho'); // Seção do carrinho
+
+menuCarToggle.addEventListener('click', () => {
+    carrinho.classList.toggle('show'); // Exibir/ocultar o carrinho ao clicar no ícone do carrinho
+});
+
+closeCartIcon.addEventListener('click', () => {
+    carrinho.classList.remove('show'); // Oculta o carrinho ao clicar no ícone de fechar
+});
+
